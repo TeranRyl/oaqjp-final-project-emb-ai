863aaf9b96fddee32c912bccabfaed6d39ff383b
TeranRyl <pachitoteranroman@gmail.com>
2025-04-05 04:09:32 -0400

    Fotmat the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
@@ -5,4 +6,20 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(url, headers=header, json=input, timeout=2.5)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions_score = formatted_response["emotionPredictions"][0]["emotion"]
+    emotion_key = "anger"
+    dominant_emotion  = {emotion_key: emotions_score[emotion_key]}
+    for emotion in emotions_score:
+        if emotions_score[emotion] > dominant_emotion[emotion_key]:
+            dominant_emotion = {emotion: emotions_score[emotion]}
+            emotion_key = emotion
+    output_format = {
+        "anger": emotions_score["anger"],
+        "disgust": emotions_score["disgust"],
+        "fear": emotions_score["fear"],
+        "joy": emotions_score["joy"],
+        "sadness": emotions_score["sadness"],
+        "dominant_emotion": emotion_key
+    }
+    return output_format
